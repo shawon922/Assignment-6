c2ebf5ba5294225ba7b96ed1a44f19b2ed6b64be
Md Mahfuzur Rahman <shawon922@gmail.com>
2015-02-07 10:20:30 +0600

    Reverse an array and add a new item to array in JS

---
+++ reverse_add_item.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+	var arr = [1, 2, 3, 4, 5];
+	
+	//reverse the array
+	arr.reverse();	
+	console.log(arr);
+	
+	//add an item
+	arr.push(7);
+	console.log(arr);
+	
+</script>
