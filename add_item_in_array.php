74b6ec7ececf0f426daa5d5d4fe89a0229ddb0d4
Md Mahfuzur Rahman <shawon922@gmail.com>
2015-02-07 10:31:08 +0600

    Add a new item to an array in php

---
+++ add_item_in_array.php
@@ -0,0 +1,10 @@
+    <?php
+    $nums = array(1, 2, 3, 4, 5);
+     
+    //Before adding a new element
+    print_r($nums);
+     
+    $nums[] = 9;
+     
+    //After addin a new element
+    print_r($nums);
